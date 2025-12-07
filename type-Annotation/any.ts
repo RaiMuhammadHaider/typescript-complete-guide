4879e7cd4beb99f1a026ce1354ee85e3ada51acf
Rai Muhammad Haider <raimuhammadalihaider@gmail.com>
2025-12-07 22:04:25 +0500

    any in typeScript

--- type-Annotation/any.ts
+++ type-Annotation/any.ts
@@ -1,6 +1,10 @@
-let anyThing : any = 'ali' // any basiclly says anythig can be value , it can be any value and it can be converted into any other kind of value its dont care
-anyThing = false
-anyThing = 393
-anyThing = {}
-anyThing = [3,4,6,67,7]
+let anyThing: any = 'ali';  
+// `any` basically disables TypeScript's type checking.
+// The variable can store ANY value and can change to ANY type.
+
+anyThing = false;         // boolean
+anyThing = 393;           // number
+anyThing = {};            // object
+anyThing = [3, 4, 6, 67]; // array
+
 console.log(anyThing);
