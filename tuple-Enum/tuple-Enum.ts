2d7ab7944e435e086f96543164f7f53159d418e1
Rai Muhammad Haider <raimuhammadalihaider@gmail.com>
2025-12-08 17:07:26 +0500

    tuple and enum in TypeScript

---
+++ tuple-Enum/tuple-Enum.ts
@@ -0,0 +1,4 @@
+// tuple 
+const ali : [number , string , number , boolean] = [1,'car' , 4, true] // touple ak asa array hota ha  jis ki length fix hoti ha or or us ma hm define krty hn har ak elemmet  ki type ko yani is ka first elemt ak number type ka ho ga second string type ka 3rd number type ka isi terha soo on
+
+// const zahid : [string , number] = [0, 'hi'] // ager thek values galat order ma pass kry gy tu ya error bi dy ga 
