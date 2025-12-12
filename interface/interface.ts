ccbc855dbcf4ca13ef94ff1519732489027ecd22
Rai Muhammad Haider <raimuhammadalihaider@gmail.com>
2025-12-13 00:17:10 +0500

    method in interface

--- interface/interface.ts
+++ interface/interface.ts
@@ -10,7 +10,11 @@ interface anyTypeName {
  age : number,
  isMarried ?: boolean // now this one is optional  
  anyFunctionOrMethod : ()=> string  // this one is the method that retun string 
+functionThatTakeParameter(amount : number) : number // this will take parameter and then retun a number so anykind of function can be make here
+
 }
-const emplyee : anyTypeName = { id : 78979798 , age : 997979 , employee : 'haris' , anyFunctionOrMethod : ()=> {return 'funn'}} 
+const emplyee : anyTypeName = { id : 78979798 , age : 997979 , employee : 'haris' , anyFunctionOrMethod : ()=> {return 'funn'}, functionThatTakeParameter(price : number){
+    return 848 + 2 // 
+} } 
 emplyee.age = 884884
 // emplyee.id = 884884 // this will never allow because it is only read only property
